cdfce2b7d87f25f6ed7a4dd7ee588bc03b21e5e0
deepakgudla <thedeepak1357@gmail.com>
2022-07-11 14:46:30 +0530

    One contract added

---
+++ HelloWorld.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+
+
+    function print() public view returns(string memory) {
+        return "hello world";
+    }
+}
+
+
+/**
+basic hello world progam..
+
+ */
