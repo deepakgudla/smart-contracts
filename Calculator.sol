1e322fc676fcfe465c93242b28a8e5184d57724c
deepakgudla <thedeepak1357@gmail.com>
2022-07-11 15:13:53 +0530

    .

--- Calculator.sol
+++ Calculator.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-//this is a simple calculator program where for every arithmetic operation there is a function decalred.....
+//this is a simple calculator program where for every arithmetic operation there is a function declared....
 // a b c are state variables..
 
 
@@ -13,22 +13,26 @@ contract Calculator {
 
 }
 
+//addition function
 function add() public view returns (uint) {
     uint p = a + b + c;
-    return p;
+    return p;  //prints 6
     
 }
 
+//subtraction function
 function subtract() public view returns(uint) {
     uint q = c - a;
     return q;
 }
 
+//multiplication function
 function multiply() public view returns(uint) {
     uint r  = a * b * c;
     return r;
 }
 
+//division function..
 function divison() public view returns(uint) {
     uint s = c / a;
     return s;
