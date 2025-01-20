91093d0a41c5faa40ca000c3858af3e678e0b34d
Deepak Gudla <44751447+deepakgudla@users.noreply.github.com>
2025-01-20 22:03:29 +0530

    Update calc.sol

--- calc.sol
+++ calc.sol
@@ -2,15 +2,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /*
-* an abstract base contract callled calculator with a read only function that returns integers
+* an abstract base contract called calculator with a read only function that returns integers
 * creating a derived contract called Test which derives the calculator contract and can calculate 1 + 2 and return the result
- */
+*/
 
  abstract contract Calculator {
     function add() public view virtual returns(uint);
+}
 
-
- }
 contract Test is Calculator {
     function add() public view override returns(uint) {
         uint a = 2;
@@ -19,6 +18,3 @@ contract Test is Calculator {
         return result;
     }
 }
- 
- 
-
