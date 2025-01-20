cf8a6af6332412e0ccf81b1f44e332025df74bb9
Deepak Gudla <44751447+deepakgudla@users.noreply.github.com>
2025-01-20 22:17:26 +0530

    Update subcurrency.sol

--- subcurrency.sol
+++ subcurrency.sol
@@ -1,15 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
+/*
 functionality of the code:
 *   this smart contract allows only its creator to create a new coin
     with different issuance shemes..
 *   anyone can send coins to eachother without a need for regstering with a 
     username and password, bu a eth keypair is needed....
 */
-
-//lesson 126
 
 contract Coin {
 
@@ -18,11 +16,11 @@ contract Coin {
 
     event Sent(address from, address to, uint amount);
 
-
     constructor() {
         minter = msg.sender;
     }
-//with minting make new coins and send them to an address
+
+//with minting, make new coins and send them to an address
 // only  owner can send coins..
 
     function mint(address receiver, uint amount) public  {
